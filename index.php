0fa792aad17d77f4af56ccdada874edff6aca8f4
Marta Domagała <mahomegol@gmail.com>
2023-08-01 16:20:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osadnicy - gra przeglądarkowa</title>
+</head>
+<body>
+   Tylko martiw ujrzeli koniec wojny - Platon
+   
+    <form action="login.php" method="post">
+    <p>Login: <input type="text" name="login">
+    </p>
+    <p>Password: <input type="password" name="password">
+    </p>
+    <input type="submit" value="Log in" >
+    </form>
+
+
+</body>
+</html>
