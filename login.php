44066a722c26967f4992c8dbfd65d94deaf89a82
Marta Domagała <mahomegol@gmail.com>
2023-08-01 18:18:04 +0200

    Complete registration form

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php
 
     session_start();
+
+    if((!isset($_POST['login'])) ||(!isset($_POST['password'])))
+    {
+        header('Location: index.php');
+        exit();
+    }
+
     require_once "connect.php";
 
    $connection = @new mysqli($host, $db_user, $db_password, $db_name);
